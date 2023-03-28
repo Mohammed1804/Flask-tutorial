7ec204f593e8c25987ad1377355b0feb2f84bdc9
Mohammed1804 <101636555+Mohammed1804@users.noreply.github.com>
2023-03-28 23:37:58 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+#name is a predefined variable
 from app import routes
+#application importing routes (placed at the bottom to workaround circular imports)
