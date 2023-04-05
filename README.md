19deeca9b5f9cee71f8f40125f9bc3d022971e8f
Mohammed1804 <101636555+Mohammed1804@users.noreply.github.com>
2023-04-05 13:20:00 +0100

    Following

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-Debugging errors
-1) error ocurs when you try and change username into one that is already registered by another user
-2) get stack trace sent to email (config)
-3) Created an SMTPhandler instance reports errors and not warnings, informational or debugging messages.
-4) tested using smtp debugging server
-5) Set a real email server *export MAIL_SERVER=smtp.googlemail.com
-export MAIL_PORT=587
-export MAIL_USE_TLS=1
-export MAIL_USERNAME=<your-gmail-username>
-export MAIL_PASSWORD=<your-gmail-password>
-6) create a log file for the application for failures that dont cause app to crash but are useful for debugging purposes
-7) Fixing username error by checking the changed username is not in the database but if user does not change their username the database should allow it (forms)
+1) in order to create a many to many relationship (to create a following concept) the database needs an additional table aka 'association-table'
+2) add 'followers' to a database (models) & declare many to many relationships in users table
+3) implement the "follow" and "unfollow" functionality as methods in the User model.
+4) logged in users should be able to see the posts they follow in index page
+5) users can see their own posts
+6) execute unnit testing
--- app/models.py
+++ app/models.py
@@ -4,6 +4,13 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 # Generic statements for login (mixin)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,16 +20,23 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140)) #add two new fields to the database table
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+#db.relationship links User instances to User instances
+#secondarry is association table
+        primaryjoin=(followers.c.follower_id == id),
+#links parernt class to other class with the association table (follower to following)
+        secondaryjoin=(followers.c.followed_id == id),
+#Links follwed to association table
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+#backref indicates how the followed links to followers
+#lazy = execution mode for query
+#dynamic sets up query not run until requested
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
-
-
-#class User(AnonymousUserMixin):
-
-
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -32,6 +46,31 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+#both follow and unfollow use append/remove and is following is used so there are no duplicates in database
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+#The join query in this instance creates an association table connecting posts and follwersto the posts
+#Followed id is = posts, database takes info from posts and attach it to the follwers table
+#join operation gives a lisst of posts followed by some users but more than needed and this is sorted by filter
+#filter only keeps in joined table that have follower_id so only info kept is from the person being followed
+#results sorted by the timestamp field of the post in descending order.
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        # user is able to see their own posts
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 #tells flask-login to get unique identify to determain if user is logged in
 @login.user_loader
@@ -46,3 +85,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+
